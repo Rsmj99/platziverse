27176a6a5591c43258c3718fb05a8a27d013ee6d
Roger Muguruza <rogermuguruza1999@gmail.com>
2021-08-06 23:31:23 -0500

    Primer commit del proyecto completo

---
+++ platziverse-web/config.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+  endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
+  apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNjI4MjExMjU3fQ.ZA-cBqncSv_ixIgahRbwiae28PbM0mihLPQbiMMWWFE'
+}
